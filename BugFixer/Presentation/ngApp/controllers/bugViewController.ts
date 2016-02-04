150abb1de445b2639be49d345eeb87d41a8356aa
brianvital <brianvital@hotmail.com>
2016-02-04 14:49:26 -0600

    bug view/controller

---
+++ BugFixer/Presentation/ngApp/controllers/bugViewController.ts
@@ -0,0 +1,38 @@
+﻿namespace BugFixer.Controllers {
+
+    export class BugViewController {
+
+        public bugs;
+        public priority;
+        public priorityLevels = [
+            {
+                name: "Critical",
+                value: 1
+            },
+            {
+                name: "High",
+                value: 2
+            },
+            {
+                name: "Medium",
+                value: 3
+            },
+            {
+                name: "Low",
+               value: 4
+            }
+        ];
+
+        constructor(private $http: ng.IHttpService, private $routeParams) { }
+
+        public addBug(title, assignedUser, description, priorityLevel) {
+            this.$http.post("/api/bug", {
+                title: title,
+                assignedUser: assignedUser,
+                description: description,
+                priorityLevel: priorityLevel
+            })
+        }
+    }
+}
+}
