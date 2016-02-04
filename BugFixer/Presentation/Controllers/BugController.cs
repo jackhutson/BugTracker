ef1578fdb506a025486d0cda5a8f2940fd6c5a27
Genny Ocampo <gegex08@aol.com>
2016-02-04 11:59:48 -0600

    Adding WebAPI Controller

---
+++ BugFixer/Presentation/Controllers/BugController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace BugFixer.Presentation.Controllers {
+    public class BugController : ApiController {
+        private BugService _bugService;
+
+        public BugController(BugService _bugService) {
+            _bugService = bugService;
+        }
+
+        [HttpPost]
+        [Route("api/")]
+        public void AddBug(int id) {
+            return _bugService.AddBug(id);
+        }
+    }
+
+}
